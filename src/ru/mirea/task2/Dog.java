7b004978e7d769c9d5b8d6ff232ca32fe97365cc
kolya <horov.kolya@yandex.ru>
2022-09-06 22:16:29 +0300

    Почти выполненное 2ое задание

---
+++ src/ru/mirea/task2/Dog.java
@@ -0,0 +1,29 @@
+package ru.mirea.task2;
+
+public class Dog {
+    private String dog_name;
+    private int dog_age;
+    public Dog(String name, int age){
+        dog_name=name;
+        dog_age=age;
+    }
+    public void setDog_name(String dog_name){
+        this.dog_name=dog_name;
+    }
+    public String getDog_name(){
+        return dog_name;
+    }
+    public void setDog_age(int dog_age){
+        this.dog_age=dog_age;
+    }
+    public int getDog_age(){
+        return dog_age;
+    }
+    public int dog_age_to_human(){
+        return dog_age*7;
+    }
+    @Override
+    public String toString(){
+        return "Кличка: "+ dog_name +"Возраст: "+dog_age+"Возраст в человеческих годах: "+dog_age_to_human();
+    }
+}
---
+++ src/ru/mirea/task2/Shape.java
@@ -0,0 +1,17 @@
+package ru.mirea.task2;
+
+public class Shape {
+    private String shape_t;
+    public Shape(){
+        shape_t=null;
+    }
+    public Shape(String shape_t){
+        this();
+        this.shape_t=shape_t;
+    }
+    @Override
+    public String toString(){
+        return shape_t;
+    }
+
+}
